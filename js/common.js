0d487e789f16922edfb35ff2bb4d9f10db90fca6
neil <neil.ling@foxmail.com>
2019-11-09 12:29:33 +0800

    Initialize the git repository.
    Signed-off-by: neil <neil.ling@foxmail.com>

---
+++ js/common.js
@@ -0,0 +1,24 @@
+const tsessionid = this.$cookies.get("tsessionid");
+let username = this.$cookies.get("nick_name");
+const userId = this.$cookies.get("user_id");
+const store = new Vuex.Store({
+    state:{
+        tsessionid,
+        groupId:3,
+        username,
+        notLogin:false,
+        userId
+    },
+    mutations:{
+        showname(state){
+            if(!state.username){
+                // location.href = '../../login.html';
+                state.notLogin = false
+            }else{
+                state.notLogin = true
+            }
+        }
+
+    }
+})
+
